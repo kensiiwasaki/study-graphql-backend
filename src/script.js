d658f38d5a7414ca7b25271a173e577ed00a5665
kensiiwasaki <kensiiwasaki@gmail.com>
2023-06-08 15:40:51 +0900

    create処理の記述

--- src/script.js
+++ src/script.js
@@ -2,3 +2,12 @@
 const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient();
+
+async function main() {
+  const newLink = await prisma.link.create({
+    data: {
+      description: 'GraphQLチュートリアルをUdemyで学ぶ',
+      url: 'www.udemy-graphql-tutorial.com',
+    },
+  });
+}
