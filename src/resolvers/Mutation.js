f3054e8543a15773dd45ca57f6464ea78bb5b7d8
kensiiwasaki <kensiiwasaki@gmail.com>
2023-07-29 16:58:40 +0900

    create token

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,5 +1,8 @@
 // ユーザの新規登録
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+APP_SECRET = 'Graphql';
 
 async function signup(parent, args, context) {
   // パスワードの設定(ハッシュ化)
@@ -12,4 +15,11 @@ async function signup(parent, args, context) {
       password,
     },
   });
+
+  const token = jwt.sign({ userId: user.id }, APP_SECRET);
+
+  return {
+    token,
+    user,
+  };
 }
