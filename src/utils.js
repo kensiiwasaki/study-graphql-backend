8ecfe47cb5a3f4a6ad0b1c18de0c79c9bf6bf2f3
kensiiwasaki <kensiiwasaki@gmail.com>
2023-07-29 17:32:58 +0900

    getUserId function

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,12 @@
+const jwt = require('jsonwebtoken');
+
+APP_SECRET = 'Graphql';
+
 // トークンを複合するための関数
+function getTokenPayload(token) {
+  // トークン化されたものの前の情報(user.id)を複合する
+  return jwt.verify(token, APP_SECRET);
+}
 
 // ユーザーIDを取得するための関数
 function getUserId(req, authToken) {
@@ -12,6 +20,18 @@ function getUserId(req, authToken) {
         throw new Error('トークンがみつかれませんでした');
       }
       // トークンの複合する
+      const { userId } = getTokenPayload(token);
+      return userId;
     }
+  } else if (authToken) {
+    const { userId } = getTokenPayload(authToken);
+    return userId;
   }
+
+  throw new Error('認証権限がありません');
 }
+
+modules.exports = {
+  APP_SECRET,
+  getUserId,
+};
